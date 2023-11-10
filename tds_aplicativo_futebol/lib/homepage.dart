96863eda10c9b920a501ab3c33e349cb1e38b9ca
Yuri Flausino <yuriflausino@alunos.utfpr.edu.br>
2023-11-10 17:21:04 -0300

    att

--- tds_aplicativo_futebol/lib/homepage.dart
+++ tds_aplicativo_futebol/lib/homepage.dart
@@ -6,27 +6,80 @@ class SportSelectionPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Selecione um Esporte'),
+        title: Text('Bem vindo Yuri'),
+        centerTitle: true,
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          InkWell(
-            onTap: () {
-              // Ao clicar na imagem, navegue para a tela SoccerApp
+          SportButton(
+            onPressed: () {
+              // Ao clicar em "Futebol", navegue para a tela SoccerApp
               Navigator.push(
                 context,
                 MaterialPageRoute(builder: (context) => SoccerApp()),
               );
             },
-            child: Image.asset(
-              'assets/images/brasileirao.png', // Caminho para a sua imagem
-              width: 150, // Ajuste conforme necessário
-              height: 150, // Ajuste conforme necessário
-            ),
+            sportName: 'Brasileirão 2023',
+            sportIcon: Icons.sports_soccer,
           ),
-          // Adicione mais imagens ou botões para outros esportes, se necessário
+          // Adicione mais botões para outros esportes, se necessário
+          SportButton(
+            onPressed: () {
+              // Ao clicar em outro esporte, implemente a navegação apropriada
+            },
+            sportName: 'Outro Esporte',
+            sportIcon: Icons.sports_baseball,
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class SportButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final String sportName;
+  final IconData sportIcon;
+
+  const SportButton({
+    required this.onPressed,
+    required this.sportName,
+    required this.sportIcon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          padding: EdgeInsets.all(16.0),
+          primary: Colors.blue, // Cor do botão
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              sportIcon,
+              size: 50.0,
+              color: Colors.white,
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              sportName,
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- tds_aplicativo_futebol/lib/login.dart
+++ tds_aplicativo_futebol/lib/login.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tds_aplicativo_futebol/auth_service.dart';
 import 'package:tds_aplicativo_futebol/homepage.dart';
-import 'package:tds_aplicativo_futebol/main.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -11,25 +10,39 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   TextEditingController _usernameController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
-  AuthService _authService =
-      AuthService(); // Instância do serviço de autenticação
+  AuthService _authService = AuthService();
+
+  bool _isLoading = false;
+  String _errorText = ''; // Nova variável para armazenar a mensagem de erro
 
   void _login() async {
+    if (_isLoading) return;
+
+    setState(() {
+      _isLoading = true;
+      _errorText =
+          ''; // Limpar a mensagem de erro ao tentar fazer login novamente
+    });
+
     String username = _usernameController.text;
     String password = _passwordController.text;
 
-    // Chame o método de autenticação do serviço
     bool isAuthenticated = await _authService.authenticate(username, password);
 
+    setState(() {
+      _isLoading = false;
+    });
+
     if (isAuthenticated) {
-      // Se autenticado, navegue para a próxima tela (SoccerApp)
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => SportSelectionPage()),
       );
     } else {
-      // Exiba uma mensagem de erro se as credenciais estiverem incorretas
-      print('Login failed. Please enter valid credentials.');
+      // Definir a mensagem de erro ao falhar o login
+      setState(() {
+        _errorText = 'Senha incorreta. Por favor, tente novamente.';
+      });
     }
   }
 
@@ -46,18 +59,32 @@ class _LoginPageState extends State<LoginPage> {
           children: [
             TextField(
               controller: _usernameController,
-              decoration: InputDecoration(labelText: 'Username'),
+              decoration: InputDecoration(
+                labelText: 'Username',
+                prefixIcon: Icon(Icons.person),
+              ),
             ),
             SizedBox(height: 16.0),
             TextField(
               controller: _passwordController,
               obscureText: true,
-              decoration: InputDecoration(labelText: 'Password'),
+              decoration: InputDecoration(
+                labelText: 'Password',
+                prefixIcon: Icon(Icons.lock),
+              ),
             ),
-            SizedBox(height: 32.0),
+            SizedBox(
+                height:
+                    8.0), // Adiciona um pequeno espaço entre os campos de texto e a mensagem de erro
+            // Exibe a mensagem de erro abaixo do botão de login
+            Text(
+              _errorText,
+              style: TextStyle(color: Colors.red),
+            ),
+            SizedBox(height: 24.0),
             ElevatedButton(
-              onPressed: _login,
-              child: Text('Login'),
+              onPressed: _isLoading ? null : _login,
+              child: _isLoading ? CircularProgressIndicator() : Text('Login'),
             ),
           ],
         ),
